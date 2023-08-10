6015e26de3a39702ec4ee5f619e8392c0735d156
Hana00661 <eng.hana95@gmail.com>
2023-08-10 16:37:49 -0700

    hana commit 1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+/**
+ * main - Ertry point
+ *  
+ * Description: A C program that print with put function
+ *
+ * Return: Alawys 0 (Success)
+ *
+*/
+
+int main(void)
+{
+         puts("\"Programming is like building a multilingual puzzle");
+         return(0);
+}
