a99b461ad7623a4dafe69a889851f6f1fe8009da
Hana00661 <eng.hana95@gmail.com>
2023-08-11 15:26:51 -0700

    hi440

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  *main - Ertry point
- *Description: 'get the last degiti of a number'
+ *Description: get the last degiti of a number
  *Return: Alawys 0 (Success)
 */
 int main(void)
@@ -16,15 +16,15 @@ int main(void)
 	ld = n % 10;
 	if (ld > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5", n, ld);
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
 	else if (ld == 0)
 	{
-		printf("Last digit of %d is %d and is 0", n, ld);
+		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and less than 8 and not 0", n, ld);
+		printf("Last digit of %d is %d and less than 6 and not 0\n", n, ld);
 	}
 	return (0);
 
