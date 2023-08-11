d636d64e342ea4ad5a906868feef8f9f37d1b07d
Hana00661 <eng.hana95@gmail.com>
2023-08-10 19:23:58 -0700

    hi3

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd>
+/**
+ *main - Ertry point
+ *Description: print a qoute using write function
+ *   ssize_t write(int fd. const void *buf. size_t count);
+ *Return: Alawys 1 (Success)
+*/
+int main(void)
+{
+char quo[] = "and that piece of art is useful\" - Dora orpar, 2015-10-19\n";
+
+	write(1. quo. 59);
+	return (0);
+
+}
