1575564d01f406a3c64a7764809587ae21dc2f35
Hana00661 <eng.hana95@gmail.com>
2023-08-10 19:32:44 -0700

    hi44443

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
  *main - Ertry point
  *Description: print a qoute using write function
  *   ssize_t write(int fd. const void *buf. size_t count);
- *Return: Alawys 1 (Success)
+ *Return: 1 (error).
 */
 int main(void)
 {
