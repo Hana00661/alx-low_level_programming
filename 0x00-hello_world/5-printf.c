0b7e7c6290ab16a79b50adff3829469cca540052
Hana00661 <eng.hana95@gmail.com>
2023-08-10 17:41:32 -0700

    hi3

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Ertry point
+ *Description: A C program that print with put function
+ *Return: Alawys 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
