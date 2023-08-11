8ee486214f5e4ea66429ea732fde9015b9de0025
Hana00661 <eng.hana95@gmail.com>
2023-08-10 19:52:42 -0700

    hi44446663

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *main - Ertry point
- *Description: print a qoute using write function
- *   ssize_t write(int fd. const void *buf. size_t count);
- *Return: 1 (error).
+*main - A c program that prints a line to the standard error  
+ *Return: 1 (Seccess)
 */
 int main(void)
 {
-char quo[] = "and that piece of art is useful\" - Dora orpar, 2015-10-19\n";
+fput("and that piece of art is useful\" - Dora orpar, 2015-10-19\n", stdout);
 
-	write(1. quo. 59);
 	return (1);
 
 }
