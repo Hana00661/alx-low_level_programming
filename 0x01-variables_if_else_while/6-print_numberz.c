8ab11e3421825b153204eb2255e44fce82daf6dd
Hana00661 <eng.hana95@gmail.com>
2023-08-11 17:21:01 -0700

    hi7773

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Ertry point
+ *Description: print 0-9 using putchar
+ *		while using int variable
+ *Return: Alawys 0 (Success)
+*/
+int main(void)
+{
+int digit = 0;
+
+	while (digit <= 9)
+	{
+		/*convert digit to ASCII representation*/
+		putchar(digit + '0');
+		digit++;
+	}
+putchar('\n');
+return (0);
+}
