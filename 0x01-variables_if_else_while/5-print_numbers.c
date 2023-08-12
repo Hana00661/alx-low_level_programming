144e31b12f7c16be8490fd843a15d5392797cc18
Hana00661 <eng.hana95@gmail.com>
2023-08-11 17:04:50 -0700

    hi3

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Ertry point
+ *Description: print all single digit numbers
+  *Return: Alawys 0 (Success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i",digit);
+		digit++;
+	}
+printf("\n");
+return (0);
+}
