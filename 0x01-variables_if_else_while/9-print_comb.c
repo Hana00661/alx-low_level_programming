d5a5bf794c43b4d960ab0ea4e751afeb06b97c56
Hana00661 <eng.hana95@gmail.com>
2023-08-11 18:01:59 -0700

    hanam773

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - Ertry point
+ *Description: print 0, 1, -0
+  *Return: Alawys 0 (Success)
+*/
+int main(void)
+{
+int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+putchar('\n');
+return (0);
+}
