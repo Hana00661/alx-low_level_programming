10960b780b4fd5d82e0b80806611ee0f46acce40
Hana00661 <eng.hana95@gmail.com>
2023-08-13 15:30:31 -0700

    h1111111i3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 /**
- *main - A c program that prints a line to the standard error
- *Return: 1 (Seccess)
+ *main - Entry point
+ *Description: A c program that prints a line to the standard error
+ *Return: 1 (error)
 */
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
 
-	write(1, quo, 59);
+	write(2, quo, 59);
 return (1);
 }
