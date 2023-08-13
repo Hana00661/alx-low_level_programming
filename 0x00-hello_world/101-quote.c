d222c5334cb5bbaff49764a132fa841b009ae6a9
Hana00661 <eng.hana95@gmail.com>
2023-08-13 16:03:49 -0700

    h1111111i3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
  *main - Entry point
@@ -6,8 +7,7 @@
 */
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
 
-	write(2, quo, 59);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 return (1);
 }
