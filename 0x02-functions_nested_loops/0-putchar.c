076d4e4ff61d041242aaa9d850a85fefcca9eae0
Hana00661 <eng.hana95@gmail.com>
2023-08-15 12:48:14 -0700

    hi3

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ *Discription: print the _putchar  function using prototype
+ *Return: 0 success
+*/
+int main(void)
+{
+	char srt[] = "_putchar);
+
+	int cha;
+	for (cha = 0; cha < 8; cha++)
+		_putchar(str[cha]);
+	_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio>
+int _putchar(char C);
+
+#endif
