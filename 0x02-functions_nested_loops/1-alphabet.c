ca97f4f76da966caa88a97149f7dc479a82c6e8c
Hana00661 <eng.hana95@gmail.com>
2023-08-15 13:49:05 -0700

    hi3

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - Ertry point
+ *print_alphabet -  utilizes on the _putchar function to print
+ *			the alphabet a to z
+*/
+void print_alphabet(void)
+{
+	int cha;
+
+	for (cha = 'a'; cha <= 'z'; cha++)
+
+		_putchar(cha);
+
+	_putchar('\n');
+}
