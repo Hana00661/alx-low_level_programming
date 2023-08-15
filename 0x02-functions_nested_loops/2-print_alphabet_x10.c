114d1c215ee2e1f6b6f32c9f2e1cd1ae2f28a111
Hana00661 <eng.hana95@gmail.com>
2023-08-15 14:37:30 -0700

    hi3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that will print the alphabet 10 times
+ * Return: Alawys 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+	int line, cha;
+
+	for (line = 0; line <= 9; line++)
+	{
+	for (cha = 'a'; cha <= 'z'; cha++)
+		       _putchar(cha);
+		_putchar('\n');
+	}
+}
