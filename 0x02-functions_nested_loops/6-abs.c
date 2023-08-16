2bc457fd3d1b6df66ca72ccbfc79acf632645b5d
Hana00661 <eng.hana95@gmail.com>
2023-08-15 17:10:34 -0700

    hwwggggi3

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,7 +4,7 @@
  * _abs - function that computes the absolute
  *		value of an integer
  *
- *@n: takes the number type is integer
+ * @n: takes the number type is integer
  *Rrturn: Return: Alawys 0 (Success)
 */
 int _abs(int n)
