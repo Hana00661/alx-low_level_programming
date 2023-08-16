6c8efbc1cb9d2d11c2baa23818fe34667dd95464
Hana00661 <eng.hana95@gmail.com>
2023-08-16 09:38:44 -0700

    hiffffffffffrrrrrrrrrrrhno3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -33,7 +33,7 @@ int main(void)
 	int count, ini0s;
 	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
-	for (count = 1; count <= 98, count++)
+	for (count = 1; count <= 98; count++)
 	{
 		if (f1o > 0)
 			printf("%lu", f1o);
