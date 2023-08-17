8fe15853254a5927553883f8429ef79904e15bc1
Hana00661 <eng.hana95@gmail.com>
2023-08-17 16:09:49 -0700

    yaammeennqjjno3

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,21 +3,25 @@
 /**
  * print_number -  a function that prints an integer.
  *
- * @n: number that will be printed
+ * @n: integer that will be printed.
 */
 
 void print_number(int n)
 {
 	unsigned int number = n;
 
+	/*first ckeck if its negative*/
 	if (n < 0)
 	{
 		_putchar('_');
 		number = -number;
 	}
 
+	/*print the first few digit*/
 	if ((number / 10) > 0)
 		print_number(number / 10);
 
+	/*print the last digit*/
 	_putchar((number % 10) + 48);
 }
+
