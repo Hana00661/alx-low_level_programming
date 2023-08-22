23ea1332cb137947eb1cc7e661581060ce8a5d97
Hana00661 <eng.hana95@gmail.com>
2023-08-22 04:11:39 -0700

    yehhhhhho33

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,3 +6,21 @@
  * @s: string parameter
  *
  * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	for (l = 0; s[l] != '\0'; ++l)
+		;
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
+
+
+}
