122d4f0b1e7bc103c066b0ff86d6d841abb1029b
Hana00661 <eng.hana95@gmail.com>
2023-09-01 12:38:33 -0700

    yesoo33

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ *
+ * @argc: integer
+ * @argv: list
+ *
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
