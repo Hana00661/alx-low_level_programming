2c5d775489db8a0b3ba96601a782fdd60a279dab
Hana00661 <eng.hana95@gmail.com>
2023-09-07 14:39:28 -0700

    hi me

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 	/*copy 1st string*/
-	for (j = 0; s1[i] != '\0' ; i++)
+	for (i = 0; s1[i] != '\0' ; i++)
+		str[i] = s1[i];
+	/*copy second string*/
+	for (j = 0; j < n; j++)
 	{
 		str[i] = s2[j];
 		i++;
