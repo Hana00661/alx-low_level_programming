87a518bac2900cce15ffbd74d7bf63c9de07ac47
Hana00661 <eng.hana95@gmail.com>
2023-09-11 14:12:48 -0700

    hi hana

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name : char
+ * @age : float
+ * @owner : char
+ *
+ * Description: alone struct dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *onwer;
+};
+
+#endif
