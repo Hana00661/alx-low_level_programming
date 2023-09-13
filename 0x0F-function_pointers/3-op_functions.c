5864c4b01326d9e7736c30ff6f9e5f67d2c09dbb
Hana00661 <eng.hana95@gmail.com>
2023-09-13 13:15:52 -0700

    hi 100000

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-clac.h"
+
+/**
+ * op_add - add oparation
+ *
+ * @a: the first num
+ * @b: the second num
+ *
+ * Return: the sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub oparation
+ *
+ * @a: the first num
+ * @b: the second num
+ *
+ * Return: the subtraction
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiblay oparation
+ *
+ * @a: the first num
+ * @b: the second num
+ *
+ * Return: the prodect
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - dividing oparation
+ *
+ * @a: the first num
+ * @b: the second num
+ *
+ * Return: the quotient
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - module
+ *
+ * @a: the first num
+ * @b: the second num
+ *
+ * Return: the reminder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
