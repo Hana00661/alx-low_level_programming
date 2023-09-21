baa4520d5713f4b815aaf3382f2a03e9ecf598e7
Hana00661 <eng.hana95@gmail.com>
2023-09-21 05:27:57 -0700

    realy confuse

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all the elements of a list_t list.
+ * @h: pointer to list
+ * Return: the number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	const list_t *ptr = h;
+	size_t count = 0;
+
+	while (ptr != NULL)
+	{
+		if (ptr->str != NULL)
+			printf("%s\n", ptr->str);
+		else
+			printf("[0] (nil)\n");
+		count++;
+		ptr = ptr->next;
+	}
+	return ((unsigned long)count);
+}
