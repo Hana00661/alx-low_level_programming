e7767e8c01d1757951eeb3d373fc7a4f563e9c99
Hana00661 <eng.hana95@gmail.com>
2023-09-29 13:36:42 -0700

    commit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_bit - Bit is set at a given index to 1
+ * @n: pointer  the number to change
+ * Return: 1 for success, -1 for failure
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
+}
