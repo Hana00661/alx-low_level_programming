be7d3e1928efd3f4faeb587e5c9e984c792b196a
Hana00661 <eng.hana95@gmail.com>
2023-09-29 14:23:20 -0700

    mooooore errrooorrssss

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,16 +3,18 @@
 
 /**
  * get_endianness - a function that checks the endianness
- * Returns: 0 if big endian, 1 if little endian
+ * Description: union user-defined data type
+ * Return: 0 if big endian, 1 if little endian
  */
 
 int get_endianness(void)
 {
-	union {
+	/* @union: user-defined data type */
+	union
+	{
 	uint32_t i;
 	uint8_t c[sizeof(uint32_t)];
 	} u;
-	
 	u.i = 1;
 	return (u.c[0] == 1);
 }
